283f9ceb7709d0fd7a0f51db65359faab9aaffb1
Roven Kyle Narra <ellamateogang@gmail.com>
2025-09-13 22:32:21 +0800

    basic changes

---
+++ src/const/index.ts
@@ -0,0 +1,47 @@
+import avatar_1 from '../assets/avatar_1.png'
+import avatar_2 from '../assets/avatar_2.png'
+import avatar_3 from '../assets/avatar_3.png'
+
+export const IMAGES = [avatar_1, avatar_2]
+
+export const NAV_ITEMS: { label: string; path: string }[] = [
+  {
+    label: 'Home',
+    path: '/',
+  },
+  {
+    label: 'About',
+    path: '/about',
+  },
+  {
+    label: 'Contact',
+    path: '/contact',
+  },
+  {
+    label: 'Projects',
+    path: '/projects',
+  },
+]
+
+export const REACH_OUT = [
+  {
+    icon: 'call',
+    url: 'tel:+69455294315'
+  },
+  {
+    icon: 'email',
+    url: 'https://www.linkedin.com/in/rnarra/',
+  },
+  {
+    icon: 'github',
+    url: 'https://github.com/Tordx',
+  },
+  {
+    icon: 'x',
+    url: 'https://x.com/tomskaroven',
+  },
+  {
+    icon: 'linkedin',
+    url: 'https://www.linkedin.com/in/rnarra/',
+  },
+]
